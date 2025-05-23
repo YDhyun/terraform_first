261928d10cfd4c36b674c90ebde6fa946c288c73
YDhyun <yun980722@naver.com>
2025-05-23 12:46:40 +0900

    output count 오류 수정

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
 output "bastion_server_pubilc_ip"{
-    value = aws_instance.bastion_server.pubilc_ip
+    value = aws_instance.bastion_server[0].public_ip
 }
 
-output "jenkins_private_ip"{
-    value = aws_instance.jenkins.private_ip
+output "jenkins_private_ip" {
+  value = aws_instance.jenkins[0].private_ip
 }
 
-output "ansdoc_private_ip"{
-    value = aws_instance.ansdoc.private_ip
+output "ansdoc_private_ip" {
+  value = aws_instance.ansdoc[0].private_ip
 }
 
 output "master_private_ip" {
