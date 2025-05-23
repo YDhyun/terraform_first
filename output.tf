e5f026d45a14e8626c4fa9fac2e42b0a8dc714c0
YDhyun <yun980722@naver.com>
2025-05-23 12:38:03 +0900

    출력값 추가

--- output.tf
+++ output.tf
@@ -6,6 +6,10 @@ output "jenkins_private_ip"{
     value = aws_instance.jenkins.private_ip
 }
 
+output "ansdoc_private_ip"{
+    value = aws_instance.ansdoc.private_ip
+}
+
 output "master_private_ip" {
     value = aws_instance.k8s_nodes[0].private_ip
 }
