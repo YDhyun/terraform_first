6f5dc720cd9969f1812e6215c6f921c4f3b4e8d6
DongHyun Yun <yun980722@naver.com>
2025-05-26 12:04:18 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -189,7 +189,7 @@ systemctl enable --now jenkins
 
 
 # Maven 설치
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.zip 
+wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz 
 tar -xvzf apache-maven-3.9.9-bin.tar.gz
 mv apache-maven-3.9.9 /opt/maven
 
